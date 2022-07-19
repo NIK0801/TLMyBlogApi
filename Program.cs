01fc1ddde7bc8eeb8bd0d24f0e8f2b873ef67ece
NIK0801 <100590631+NIK0801@users.noreply.github.com>
2022-07-19 12:55:46 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,18 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<IRepository<Posts>>(s =>
-    new PostsRepository(builder.Configuration.GetValue<string>("DefaultConnection")));
-builder.Services.AddScoped<IService<Posts, PostsDto>, PostsService>();
+builder.Services.AddScoped<IRepository<Post>>(s =>
+    new PostRepository(builder.Configuration.GetValue<string>("DefaultConnection")));
+builder.Services.AddScoped<IService<Post, PostDto>, PostService>();
 
 
-builder.Services.AddScoped<IRepository<Tags>>(s =>
-    new TagsRepository(builder.Configuration.GetValue<string>("DefaultConnection")));
-builder.Services.AddScoped<IService<Tags, TagsDto>, TagsService>();
+builder.Services.AddScoped<IRepository<Tag>>(s =>
+    new TagRepository(builder.Configuration.GetValue<string>("DefaultConnection")));
+builder.Services.AddScoped<IService<Tag, TagDto>, TagService>();
 
-builder.Services.AddScoped<IRepository<Categories>>(s =>
-    new CategoriesRepository(builder.Configuration.GetValue<string>("DefaultConnection")));
-builder.Services.AddScoped<IService<Categories, CategoriesDto>, CategoriesService>();
+builder.Services.AddScoped<IRepository<Category>>(s =>
+    new CategoryRepository(builder.Configuration.GetValue<string>("DefaultConnection")));
+builder.Services.AddScoped<IService<Category, CategoryDto>, CategoryService>();
 
 var app = builder.Build();
 app.MapControllers();

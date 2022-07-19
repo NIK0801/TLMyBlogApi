7e7480759d70ac61807348e185f5661ccc308621
NIK0801 <100590631+NIK0801@users.noreply.github.com>
2022-07-19 13:00:19 +0300

    Add files via upload

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using MyBlogApi.Domain;
+
+namespace MyBlogApi.Repositories
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        int Create(T domain);
+        void Delete(T domain);
+        int Update(T domain);
+        bool Check(int Id);
+    }
+}

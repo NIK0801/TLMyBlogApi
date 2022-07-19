089c08a022ad1cc9c642a2749c6713e8f1df96fa
NIK0801 <100590631+NIK0801@users.noreply.github.com>
2022-07-19 12:59:34 +0300

    Add files via upload

---
+++ Dto/PostDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBlogApi.Dto
+{
+    public class PostDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int IsPublished { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ Extensions/CategoryExtension.cs
@@ -0,0 +1,26 @@
+﻿using MyBlogApi.Domain;
+using MyBlogApi.Repositories;
+using MyBlogApi.Dto;
+
+namespace MyBlogApi.Extensions
+{
+    public static class CategoryExtension
+    {
+        public static Category ConvertToCategories(this CategoryDto categoryDto)
+        {
+            return new Category
+            {
+                Id = categoryDto.Id,
+                Title = categoryDto.Title
+            };
+        }
+        public static CategoryDto ConvertToCategoriesDto(this Category category)
+        {
+            return new CategoryDto
+            {
+                Id = category.Id,
+                Title = category.Title
+            };
+        }
+    }
+}

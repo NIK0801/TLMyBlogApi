b2557a50e5bbea51eec4d1215b3636dbb92a5a36
NIK0801 <100590631+NIK0801@users.noreply.github.com>
2022-07-19 12:54:53 +0300

    Add files via upload

---
+++ Dto/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBlogApi.Dto
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
--- Filter/ExceptionFilter.cs
+++ Filter/ExceptionFilter.cs
@@ -3,13 +3,11 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace MyBlogApi.Filter
 {
-    public class AllExceptionAttribute : ExceptionFilterAttribute
+    public class ExceptionFilter : ExceptionFilterAttribute
     {
         public override void OnException(ExceptionContext context)
         {
             context.Result = new BadRequestObjectResult(context.Exception.Message);
         }
-
-
     }
 }

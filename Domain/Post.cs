72889068fb3edba8dd3bfe4d928adbe5b4c2e939
NIK0801 <100590631+NIK0801@users.noreply.github.com>
2022-07-20 18:14:47 +0300

    Add files via upload

---
+++ Domain/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBlogApi.Domain
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int IsPublished { get; set; }
+        public int CategoryId { get; set; } 
+    }
+}
---
+++ Dto/TagDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBlogApi.Dto
+{
+    public class TagDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
